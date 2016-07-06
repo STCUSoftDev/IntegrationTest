8ba76346caac02cbe40b278f2e8158a542474dfc
STCUSoftDev <SoftwareDevSupport@stcu.org>
2016-07-06 08:10:28 -0700

    Initial code commit.

---
+++ build.fsx
@@ -0,0 +1,11 @@
+﻿#I @"C:\Tools\FAKE\tools"
+#r "FakeLib"
+#I @"C:\Tools\StcuFake"
+#r "StcuFake"
+open Fake
+open STCU
+BuildCore.DefineDefaultTargets ()
+
+"BuildAll" ==> "Continuous"
+
+BuildCore.RunTargetOrContinuous ()
